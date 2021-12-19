9e8981ef4500a73ce0294a2e64710ea9e34e6616
Thorsten Behrens <yorickdowne@gmail.com>
2021-12-19 16:23:13 +0000

    auto-prune.sh outside eth-docker

--- auto-prune.sh
+++ auto-prune.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+percent_threshold=10
+byte_threshold=104857600
 
 if [ $(dpkg-query -W -f='${Status}' bc 2>/dev/null | grep -c "ok installed") = "0" ]; then
   echo "This script requires the bc package, please install it via 'sudo apt install bc'"
@@ -14,19 +16,18 @@ for var in "$@"; do
   fi
 done
 
-
 FREE_DISK=$(df -P /var/lib/docker/ | awk '/[0-9]%/{print $(NF-2)}')
 TOTAL_DISK=$(df -P /var/lib/docker/ | awk '/[0-9]%/{print $(NF-4)}')
 PERCENT_FREE=$(echo "percent = ($FREE_DISK / $TOTAL_DISK) * 100; scale = 0; percent / 1" | bc -l)
 FREE_DISK_GB=$(echo "$FREE_DISK / 1024 / 1024" | bc)
 
 # If under 100 GiB or 10% free, prune
-if [ $FREE_DISK -lt 104857600 -o $PERCENT_FREE -lt 10 ]; then
-   if [ ! -f "./ethd" ]; then
-      echo "Geth prune should be started, but Geth pruning script not found. Aborting."
-      exit 1
-   fi
+if [ $FREE_DISK -lt ${byte_threshold} -o $PERCENT_FREE -lt ${percent_threshold} ]; then
    if [ $dryrun = "0" ]; then
+     if [ ! -f "./ethd" ]; then
+       echo "Geth prune should be started, but Geth pruning script not found. Aborting."
+       exit 1
+     fi
      echo "Starting Geth prune. $FREE_DISK_GB GiB free on disk, which is $PERCENT_FREE percent."
      exec ./ethd prune-geth
    else
