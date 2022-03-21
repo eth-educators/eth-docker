cb83c2b19000125698cd7ec50d76de793cdaa636
Thorsten Behrens <yorickdowne@gmail.com>
2022-03-21 16:40:31 +0000

    Add Lodestar

--- geth/docker-entrypoint.sh
+++ geth/docker-entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-# Init from genesis file if needed
-if [[ ! -f /var/lib/goethereum/setupdone ]]; then
+if [[ ! -f /secrets/jwtsecret ]]; then
   __secret1=$(echo $RANDOM | md5sum | head -c 32)
   __secret2=$(echo $RANDOM | md5sum | head -c 32)
   echo -n ${__secret1}${__secret2} > /secrets/jwtsecret
-  cp /secrets/jwtsecret /var/lib/goethereum/
-  chown 10001:10001 /var/lib/goethereum/jwtsecret
-  chown 10002:10002 /secrets/jwtsecret
-  touch /var/lib/goethereum/setupdone
+fi
+
+cp /secrets/jwtsecret /var/lib/goethereum/
+chown 10001:10001 /var/lib/goethereum/jwtsecret
+chown 10002:10002 /secrets/jwtsecret
+
+# Init from genesis file if needed
+if [[ ! -f /var/lib/goethereum/setupdone ]]; then
   su-exec geth geth --datadir /var/lib/goethereum init /configs/genesis.json
+  touch /var/lib/goethereum/setupdone
 fi
 
 exec su-exec geth $@
--- lodestar/validator-import.sh
+++ lodestar/validator-import.sh
@@ -5,8 +5,24 @@ set -Eeuo pipefail
 if [ "$(id -u)" = '0' ]; then
   mkdir /val_keys
   cp /validator_keys/* /val_keys/
-  chown lsvalidator:lsvalidator /val_keys/*
+  chown -R lsvalidator:lsvalidator /val_keys
   exec su-exec lsvalidator "$BASH_SOURCE" "$@"
 fi
 
+__non_interactive=0
+if echo "$@" | grep -q '.*--non-interactive.*' 2>/dev/null ; then
+  __non_interactive=1
+fi
+for arg do
+  shift
+  [ "$arg" = "--non-interactive" ] && continue
+  set -- "$@" "$arg"
+done
+
+if [ ${__non_interactive} = 1 ]; then
+  echo ${KEYSTORE_PASSWORD} | $@
+  exit 0
+fi
+
+# Only reached in interactive mode
 exec "$@"
