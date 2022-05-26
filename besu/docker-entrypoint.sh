7693a94dbbc65a64973e214819f7c1a0f2b276b7
Thorsten Behrens <tbehrens@outlook.com>
2022-05-26 19:15:03 +0000

    Added Geth for Ropsten

--- besu/docker-entrypoint.sh
+++ besu/docker-entrypoint.sh
@@ -3,11 +3,11 @@ set -Eeuo pipefail
 
 if [ -n "${JWT_SECRET}" ]; then
   echo -n ${JWT_SECRET} > /var/lib/besu/secrets/jwtsecret
-  echo "Secret was supplied"
+  echo "JWT secret was supplied in .env"
 fi
 
 if [[ ! -f /var/lib/besu/secrets/jwtsecret ]]; then
-  echo "Writing secret"
+  echo "Generating JWT secret"
   __secret1=$(echo $RANDOM | md5sum | head -c 32)
   __secret2=$(echo $RANDOM | md5sum | head -c 32)
   echo -n ${__secret1}${__secret2} > /var/lib/besu/secrets/jwtsecret
--- geth/docker-entrypoint.sh
+++ geth/docker-entrypoint.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env sh
+if [ -n "${JWT_SECRET}" ]; then
+  echo -n ${JWT_SECRET} > /var/lib/goethereum/secrets/jwtsecret
+  echo "JWT secret was supplied in .env"
+fi
+
+if [[ ! -f /var/lib/goethereum/secrets/jwtsecret ]]; then
+  echo "Generating JWT secret"
+  __secret1=$(echo $RANDOM | md5sum | head -c 32)
+  __secret2=$(echo $RANDOM | md5sum | head -c 32)
+  echo -n ${__secret1}${__secret2} > /var/lib/goethereum/secrets/jwtsecret
+fi
+
 if [ -f /var/lib/goethereum/prune-marker ]; then
   $@ snapshot prune-state
   rm -f /var/lib/goethereum/prune-marker
