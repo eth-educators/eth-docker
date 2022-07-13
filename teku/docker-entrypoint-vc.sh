f5d03e42e47c7cdfc0145f09bb000065822cf6e2
Thorsten Behrens <yorickdowne@gmail.com>
2022-07-13 08:59:14 +0000

    Add MEV boost support for Prysm

--- teku/docker-entrypoint-vc.sh
+++ teku/docker-entrypoint-vc.sh
@@ -12,32 +12,12 @@ if [ ! -f /var/lib/teku/teku-keyapi.keystore ]; then
     openssl pkcs12 -export -in /var/lib/teku/teku-keyapi.crt -inkey /var/lib/teku/teku-keyapi.key -out /var/lib/teku/teku-keyapi.keystore -name teku-keyapi -passout pass:$__password
 fi
 
-if [ -n "${JWT_SECRET}" ]; then
-  echo -n ${JWT_SECRET} > /var/lib/teku/secrets/jwtsecret
-  echo "JWT secret was supplied in .env"
-fi
-
-# Check whether we should rapid sync
-if [ -n "${RAPID_SYNC_URL:+x}" ]; then
-  __rapid_sync="--initial-state=${RAPID_SYNC_URL}/eth/v1/debug/beacon/states/finalized"
-else
-  __rapid_sync=""
-fi
-
-# Check whether we should override TTD
-if [ -n "${OVERRIDE_TTD}" ]; then
-  __override_ttd="--Xnetwork-total-terminal-difficulty-override=${OVERRIDE_TTD}"
-  echo "Overriding TTD to ${OVERRIDE_TTD}"
-else
-  __override_ttd=""
-fi
-
 # Check whether we should use MEV Boost
 if [ "${MEV_BOOST}" = "true" ]; then
-  __mev_boost="--Xvalidators-registration-default-enabled=true --Xeb-endpoint=http://mev-boost:18550"
+  __mev_boost="--Xvalidators-registration-default-enabled=true"
   echo "MEV Boost enabled"
 else
   __mev_boost=""
 fi
 
-exec $@ ${__mev_boost} ${__rapid_sync} ${__override_ttd}
+exec $@ ${__mev_boost}
