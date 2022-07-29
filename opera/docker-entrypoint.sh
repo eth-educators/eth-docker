f39bfdbd41b355d6eeeffa25032671bf6dc81c51
Thorsten Behrens <tbehrens@outlook.com>
2022-07-29 12:03:39 +0000

    Improved Opera genesis file handling

--- opera/docker-entrypoint.sh
+++ opera/docker-entrypoint.sh
@@ -2,13 +2,20 @@
 set -Eeuo pipefail
 
 GENESIS=/var/lib/opera/mainnet.g
-if [[ ! -f "$GENESIS" && -n "$GENESIS_FILE" ]]; then
+if [[ ! -f "/var/lib/opera/setupdone" && -n "$GENESIS_FILE" ]]; then
   echo "Fetching genesis file for mainnet"
-  wget -O "$GENESIS" "$GENESIS_FILE" 
+  wget -O "$GENESIS" "$GENESIS_FILE"
+  touch /var/lib/opera/setupdone
+  exec "$@" --genesis "$GENESIS"
 else
   echo "Genesis file not found. Please specify GENESIS_FILE in .env"
   echo "and then ./ethd restart"
   exit 0
+fi
+
+if [[ -f "/var/lib/opera/setupdone" && -f "$GENESIS" ]]; then
+  echo "Removing processed Genesis file"
+  rm "$GENESIS"
 fi
 
 exec "$@"
