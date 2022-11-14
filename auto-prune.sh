683b7819f46bf1f0763c15bc3224fe4aba87d9e6
Thorsten Behrens <yorickdowne@gmail.com>
2022-11-14 12:19:36 +0000

    Lint pass on auto-prune.sh

--- auto-prune.sh
+++ auto-prune.sh
@@ -2,12 +2,12 @@
 percent_threshold=10
 byte_threshold=104857600
 
-if [ $(dpkg-query -W -f='${Status}' bc 2>/dev/null | grep -c "ok installed") = "0" ]; then
+if [ "$(dpkg-query -W -f='${Status}' bc 2>/dev/null | grep -c "ok installed")" = "0" ]; then
   echo "This script requires the bc package, please install it via 'sudo apt install bc'"
   exit 1
 fi
 
-cd "$(dirname "$0")";
+cd "$(dirname "$0")" || exit 1
 
 dryrun=0
 for var in "$@"; do
@@ -22,7 +22,7 @@ PERCENT_FREE=$(echo "percent = ($FREE_DISK / $TOTAL_DISK) * 100; scale = 0; perc
 FREE_DISK_GB=$(echo "$FREE_DISK / 1024 / 1024" | bc)
 
 # If under 100 GiB or 10% free, prune
-if [ $FREE_DISK -lt ${byte_threshold} -o $PERCENT_FREE -lt ${percent_threshold} ]; then
+if [ "$FREE_DISK" -lt "${byte_threshold}" ] || [ "$PERCENT_FREE" -lt "${percent_threshold}" ]; then
    if [ $dryrun = "0" ]; then
      if [ ! -f "./ethd" ]; then
        echo "Geth prune should be started, but Geth pruning script not found. Aborting."
