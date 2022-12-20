459603bc8ef0ee8c1f7765e7810c8a1d05d2170f
Thorsten Behrens <yorickdowne@gmail.com>
2022-12-20 15:38:40 +0000

    auto-prune can handle Nethermind and different docker dir

--- auto-prune.sh
+++ auto-prune.sh
@@ -1,36 +1,86 @@
 #!/bin/bash
-percent_threshold=10
-byte_threshold=104857600
+__percent_threshold=10
+__byte_threshold=104857600
 
-if [ "$(dpkg-query -W -f='${Status}' bc 2>/dev/null | grep -c "ok installed")" = "0" ]; then
-  echo "This script requires the bc package, please install it via 'sudo apt install bc'"
+if [ "$(dpkg-query -W -f='${Status}' bc 2>/dev/null | grep -c "ok installed")" = "0" ] || [ "$(dpkg-query -W -f='${Status}' jq 2>/dev/null | grep -c "ok installed")" = "0" ]; then
+  echo "This script requires the bc and jq packages, please install them via 'sudo apt install bc jq'"
   exit 1
 fi
 
 cd "$(dirname "$0")" || exit 1
 
-dryrun=0
-for var in "$@"; do
+
+__docker_dir="/var/lib/docker"
+if [ -f "/etc/docker/daemon.json" ]; then
+  __dir=$(jq '."data-root"' /etc/docker/daemon.json)
+  if [ ! "$__dir" = "null" ]; then
+    __docker_dir="$__dir"
+  fi
+fi
+
+__dryrun=0
+__threshold_override=0
+for (( i=1; i<=$#; i++ )); do
+  var="${!i}"
   if [ "$var" = "--dry-run" ]; then
-    dryrun=1
+    __dryrun=1
+  fi
+  if [ "$var" = "--threshold" ]; then
+    j=$((i+1))
+    __byte_threshold=${!j}
+    __threshold_override=1
+    re='^[0-9]+$'
+    if [ -z "$__byte_threshold" ] || [[ ! "$__byte_threshold" =~ $re ]]; then
+      echo "--threshold requires a size in bytes"
+      exit 1
+    fi
+  fi
+  if [ "$var" = "--help" ]; then
+    echo "Check available space on $__docker_dir and output to stdout when it is under 100 GiB or 10%"
+    echo "Meant to be run from crontab with a MAILTO, as a simple alerting mechanism."
+    echo "For Geth 1.10.x or Nethermind, this can also kick off an automatic prune."
+    echo
+    echo "--dry-run"
+    echo "  Run and alert on diskspace, but do not start a prune"
+    echo "--threshold <bytes>"
+    echo "  Disk free threshold in bytes at which to alert"
+    exit 0
   fi
 done
 
-FREE_DISK=$(df -P /var/lib/docker/ | awk '/[0-9]%/{print $(NF-2)}')
-TOTAL_DISK=$(df -P /var/lib/docker/ | awk '/[0-9]%/{print $(NF-4)}')
+FREE_DISK=$(df -P "$__docker_dir" | awk '/[0-9]%/{print $(NF-2)}')
+TOTAL_DISK=$(df -P "$__docker_dir" | awk '/[0-9]%/{print $(NF-4)}')
 PERCENT_FREE=$(echo "percent = ($FREE_DISK / $TOTAL_DISK) * 100; scale = 0; percent / 1" | bc -l)
 FREE_DISK_GB=$(echo "$FREE_DISK / 1024 / 1024" | bc)
 
-# If under 100 GiB or 10% free, prune
-if [ "$FREE_DISK" -lt "${byte_threshold}" ] || [ "$PERCENT_FREE" -lt "${percent_threshold}" ]; then
-   if [ $dryrun = "0" ]; then
-     if [ ! -f "./ethd" ]; then
-       echo "Geth prune should be started, but Geth pruning script not found. Aborting."
-       exit 1
-     fi
-     echo "Starting Geth prune. $FREE_DISK_GB GiB free on disk, which is $PERCENT_FREE percent."
-     exec ./ethd prune-geth --non-interactive
-   else
-     echo "Disk space low, prune may be required. $FREE_DISK_GB GiB free on disk, which is $PERCENT_FREE percent."
-   fi
+# Try and detect the EL
+el=nada
+var="COMPOSE_FILE"
+value=$(sed -n -e "s/^${var}=\(.*\)/\1/p" ".env" || true)
+if [[ "${value}" =~ "geth.yml" ]]; then
+  __el=geth
+  if [ "$__threshold_override" -eq 0 ]; then
+    __byte_threshold=104857600
+  fi
+elif [[ "${value}" =~ "nethermind.yml" ]]; then
+  __el=nethermind
+  if [ "$__threshold_override" -eq 0 ]; then
+    __byte_threshold=262144000
+  fi
+fi
+
+# If under byte threshold or 10% free, alert
+if [ "$FREE_DISK" -lt "${__byte_threshold}" ] || [ "$PERCENT_FREE" -lt "${__percent_threshold}" ]; then
+  if [ "$__dryrun" -eq 0 ]; then
+    if  [ "$__el" = "geth" ] || [ "$__el" = "nethermind" ]; then
+      if [ ! -f "./ethd" ]; then
+        echo "$__el prune should be started, but $__el pruning script not found. Aborting."
+        exit 1
+      fi
+      echo "Starting $__el prune. $FREE_DISK_GB GiB free on disk, which is $PERCENT_FREE percent."
+      exec ./ethd prune-$__el --non-interactive
+    fi
+  fi
+# The previous options will have exited the script before here
+  echo "Disk space low, prune or resync may be required. $FREE_DISK_GB GiB free on disk, which is $PERCENT_FREE percent."
 fi
