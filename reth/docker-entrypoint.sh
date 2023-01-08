70f42d09d72552f9b653fa03ccaf217f33d514fa
Thorsten Behrens <yorickdowne@gmail.com>
2023-01-08 18:13:52 +0000

    Extremely experimental support for Reth

---
+++ reth/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if [ "$(id -u)" = '0' ]; then
+  chown -R reth:reth /var/lib/reth
+  exec gosu reth "${BASH_SOURCE[0]}" "$@"
+fi
+
+if [ -n "${JWT_SECRET}" ]; then
+  echo -n "${JWT_SECRET}" > /var/lib/reth/ee-secret/jwtsecret
+  echo "JWT secret was supplied in .env"
+fi
+
+if [[ ! -f /var/lib/reth/ee-secret/jwtsecret ]]; then
+  echo "Generating JWT secret"
+  __secret1=$(echo $RANDOM | md5sum | head -c 32)
+  __secret2=$(echo $RANDOM | md5sum | head -c 32)
+  echo -n "${__secret1}""${__secret2}" > /var/lib/reth/ee-secret/jwtsecret
+fi
+
+if [[ -O "/var/lib/reth/ee-secret" ]]; then
+  # In case someone specificies JWT_SECRET but it's not a distributed setup
+  chmod 777 /var/lib/reth/ee-secret
+fi
+if [[ -O "/var/lib/reth/ee-secret/jwtsecret" ]]; then
+  chmod 666 /var/lib/reth/ee-secret/jwtsecret
+fi
+
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+exec "$@" ${EL_EXTRAS}
