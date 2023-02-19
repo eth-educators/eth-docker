97fe33853b1e39ed7ead804e9363019c64c6b94e
nflaig <nflaig@protonmail.com>
2023-02-19 20:34:30 +0100

    Add support for Lodestar beaconcha.in monitoring

--- lodestar/docker-entrypoint.sh
+++ lodestar/docker-entrypoint.sh
@@ -31,6 +31,14 @@ else
   __mev_boost=""
 fi
 
+# Check whether we should send stats to beaconcha.in
+if [ -n "${BEACON_STATS_API}" ]; then
+  __beacon_stats="--monitoring.endpoint https://beaconcha.in/api/v1/client/metrics?apikey=${BEACON_STATS_API}&machine=${BEACON_STATS_MACHINE}"
+  echo "Beacon stats API enabled"
+else
+  __beacon_stats=""
+fi
+
 # Check whether we should rapid sync
 if [ -n "${RAPID_SYNC_URL}" ]; then
   if [ "${ARCHIVE_NODE}" = "true" ]; then
@@ -46,4 +54,4 @@ fi
 
 # Word splitting is desired for the command line parameters
 # shellcheck disable=SC2086
-exec "$@" ${__mev_boost} ${__rapid_sync} ${CL_EXTRAS}
+exec "$@" ${__mev_boost} ${__beacon_stats} ${__rapid_sync} ${CL_EXTRAS}
