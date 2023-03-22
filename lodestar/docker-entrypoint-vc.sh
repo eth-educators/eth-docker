5b49cdd9e29e408774514ea20465f3742edb2cdd
Nico Flaig <nflaig@protonmail.com>
2023-03-22 12:42:15 +0100

    Add support for Lodestar beaconcha.in monitoring (#1193)

--- lodestar/docker-entrypoint-vc.sh
+++ lodestar/docker-entrypoint-vc.sh
@@ -14,6 +14,14 @@ else
   __mev_boost=""
 fi
 
+# Check whether we should send stats to beaconcha.in
+if [ -n "${BEACON_STATS_API}" ]; then
+  __beacon_stats="--monitoring.endpoint https://beaconcha.in/api/v1/client/metrics?apikey=${BEACON_STATS_API}&machine=${BEACON_STATS_MACHINE}"
+  echo "Beacon stats API enabled"
+else
+  __beacon_stats=""
+fi
+
 # Check whether we should enable doppelganger protection
 if [ "${DOPPELGANGER}" = "true" ]; then
   __doppel="--doppelgangerProtectionEnabled"
@@ -24,4 +32,4 @@ fi
 
 # Word splitting is desired for the command line parameters
 # shellcheck disable=SC2086
-exec "$@" ${__mev_boost} ${__doppel} ${VC_EXTRAS}
+exec "$@" ${__mev_boost} ${__beacon_stats} ${__doppel} ${VC_EXTRAS}
