654fbf59ae5d25efef6ccc04ead592b1b98ad8d5
yorickdowne <71337066+yorickdowne@users.noreply.github.com>
2023-04-03 13:56:05 -0400

    Fix test.yml dependency (#1257)
    * Fix test.yml dependency
    
    * No need to re-lint on label add
    
    * Fix helper.sh
    
    * Fix check service name

--- .github/helper.sh
+++ .github/helper.sh
@@ -3,10 +3,10 @@
 set_value_in_env() {
     # Assumes that "var" has been set to the name of the variable to be changed
     if [ "${!var+x}" ]; then
-        if ! grep -qF "${var}" "${ENV_FILE}" 2>/dev/null ; then
-            echo "${var}=${!var}" >> "${ENV_FILE}"
+        if ! grep -qF "${var}" .env 2>/dev/null ; then
+            echo "${var}=${!var}" >> .env
         else
-            sed -i'.original' -e "s~^\(${var}\s*=\s*\).*$~\1${!var}~" "${ENV_FILE}"
+            sed -i'.original' -e "s~^\(${var}\s*=\s*\).*$~\1${!var}~" .env
         fi
     fi
 }
