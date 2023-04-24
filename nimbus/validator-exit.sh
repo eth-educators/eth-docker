caf13b09216f0af4dc24c9efccf3a76d59a65340
yorickdowne <71337066+yorickdowne@users.noreply.github.com>
2023-04-24 06:53:57 -0400

    Nimbus val exit works when there are directories in validator_keys (#1340)

--- nimbus/validator-exit.sh
+++ nimbus/validator-exit.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 # Copy keys, then restart script without root
 if [ "$(id -u)" = '0' ]; then
   mkdir /keys
-  cp /validator_keys/* /keys/
+  cp -r /validator_keys/* /keys/
   chown user:user /keys/*
   exec gosu user "${BASH_SOURCE[0]}" "$@"
 fi
