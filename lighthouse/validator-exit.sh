62a488b0d3bf75a161c36b800ab3d9c82f87cc42
Nick Sullivan <nick@sullivanflock.com>
2023-04-23 17:50:46 -0700

    Specify `-r` for `cp`, ensuring that the script will still work if the `validator_keys` directory contains a subdirectory, such as `keybackup`

--- lighthouse/validator-exit.sh
+++ lighthouse/validator-exit.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 # Copy keys, then restart script without root
 if [ "$(id -u)" = '0' ]; then
   mkdir /keys
-  cp /validator_keys/* /keys/
+  cp -r /validator_keys/* /keys/
   chown lhvalidator:lhvalidator /keys/*
   exec gosu lhvalidator "${BASH_SOURCE[0]}" "$@"
 fi
