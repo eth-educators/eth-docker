06ef38c0eb5849d0ec7350a0f4049064eec54b52
yorickdowne <71337066+yorickdowne@users.noreply.github.com>
2023-08-09 02:57:54 +0100

    Improve CI (#1474)

--- .github/check-service.sh
+++ .github/check-service.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-APP_NAME=$1
-TIMEOUT=${2:-60}  # Default timeout is 60 seconds if not provided
-INTERVAL=5
+__service=$1
 
-while [ "${TIMEOUT}" -gt 0 ]; do
-    STATUS=$(docker-compose ps --services --filter "status=running" | grep "${APP_NAME}")
+__containerID=$(docker-compose ps -q "${__service}")
 
-    if [ -n "$STATUS" ]; then
-        echo "$APP_NAME is running."
-        exit 0
-    fi
+__initial_restart_count=$(docker inspect --format '{{ .RestartCount }}' "$__containerID")
+__is_running=$(docker inspect --format '{{ .State.Running }}' "$__containerID")
+sleep 10
+__final_restart_count=$(docker inspect --format '{{ .RestartCount }}' "$__containerID")
 
-    sleep "${INTERVAL}"
-    TIMEOUT=$((TIMEOUT - INTERVAL))
-done
-
-echo "Timed out waiting for $APP_NAME to start."
-exit 1
+if [ "$__is_running" != "true" ] || [ "$__final_restart_count" -gt "$__initial_restart_count" ]; then
+  echo "$__service is either not running or continuously restarting"
+  docker-compose ps "${__service}"
+  docker-compose logs "${__service}"
+  exit 1
+else
+  echo "$__service is running"
+  docker-compose ps "${__service}"
+  exit 0
+fi
