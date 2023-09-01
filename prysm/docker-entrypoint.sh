05163b53def7cdc38fde6380f53336239dbd19fd
Yorick Downe <yorickdowne@gmail.com>
2023-09-01 10:52:59 +0000

    Prysm custom testnets

--- prysm/docker-entrypoint.sh
+++ prysm/docker-entrypoint.sh
@@ -19,6 +19,32 @@ if [[ -O "/var/lib/prysm/ee-secret/jwtsecret" ]]; then
   chmod 666 /var/lib/prysm/ee-secret/jwtsecret
 fi
 
+if [[ "${NETWORK}" =~ ^https?:// ]]; then
+  echo "Custom testnet at ${NETWORK}"
+  repo=$(awk -F'/tree/' '{print $1}' <<< "${NETWORK}")
+  branch=$(awk -F'/tree/' '{print $2}' <<< "${NETWORK}" | cut -d'/' -f1)
+  config_dir=$(awk -F'/tree/' '{print $2}' <<< "${NETWORK}" | cut -d'/' -f2-)
+  echo "This appears to be the ${repo} repo, branch ${branch} and config directory ${config_dir}."
+  # For want of something more amazing, let's just fail if git fails to pull this
+  set -e
+  if [ ! -d "/var/lib/prysm/testnet/${config_dir}" ]; then
+    mkdir -p /var/lib/prysm/testnet
+    cd /var/lib/prysm/testnet
+    git init --initial-branch="${branch}"
+    git remote add origin "${repo}"
+    git config core.sparseCheckout true
+    echo "${config_dir}" > .git/info/sparse-checkout
+    git pull origin "${branch}"
+  fi
+  bootnodes="$(paste -s -d, "/var/lib/prysm/testnet/${config_dir}/bootstrap_nodes.txt")"
+  deploy_block=$(cat "/var/lib/prysm/testnet/${config_dir}/deploy_block.txt")
+  set +e
+  __network="--chain-config-file=/var/lib/prysm/testnet/${config_dir}/config.yaml --genesis-state=/var/lib/prysm/testnet/${config_dir}/genesis.ssz \
+--enable-debug-rpc-endpoints --bootstrap-node=${bootnodes} --contract-deployment-block=${deploy_block}"
+else
+  __network="--${NETWORK}"
+fi
+
 # Check whether we should rapid sync
 if [ -n "${RAPID_SYNC_URL:+x}" ]; then
   __rapid_sync="--checkpoint-sync-url=${RAPID_SYNC_URL}"
@@ -42,31 +68,27 @@ else
   __prune=""
 fi
 
-# Fetch genesis file as needed if beacon
-if [[ "$1" =~ ^(beacon-chain)$ ]]; then
-  if [[ "$*" =~ --prater || "$*" =~ --goerli ]]; then
-    GENESIS=/var/lib/prysm/genesis.ssz
-    if [ ! -f "$GENESIS" ]; then
-      echo "Fetching genesis file for Goerli testnet"
-      curl -fsSL -o "$GENESIS" https://github.com/eth-clients/eth2-networks/raw/master/shared/prater/genesis.ssz
-    fi
-# Word splitting is desired for the command line parameters
-# shellcheck disable=SC2086
-    exec "$@" "--genesis-state=$GENESIS" ${__rapid_sync} ${__prune} ${__mev_boost} ${CL_EXTRAS}
-  elif [[ "$*" =~ --sepolia ]]; then
-    GENESIS=/var/lib/prysm/genesis.ssz
-    if [ ! -f "$GENESIS" ]; then
-      echo "Fetching genesis file for Sepolia testnet"
-      curl -fsSL -o "$GENESIS" https://github.com/eth-clients/merge-testnets/raw/main/sepolia/genesis.ssz
-    fi
-# Word splitting is desired for the command line parameters
-# shellcheck disable=SC2086
-    exec "$@" "--genesis-state=$GENESIS" ${__rapid_sync} ${__prune} ${__mev_boost} ${CL_EXTRAS}
-  else
-# Word splitting is desired for the command line parameters
-# shellcheck disable=SC2086
-    exec "$@" ${__rapid_sync} ${__prune} ${__mev_boost} ${CL_EXTRAS}
+# Fetch genesis file as needed
+if [[ "$*" =~ --prater || "$*" =~ --goerli ]]; then
+  GENESIS=/var/lib/prysm/genesis.ssz
+  if [ ! -f "$GENESIS" ]; then
+    echo "Fetching genesis file for Goerli testnet"
+    curl -fsSL -o "$GENESIS" https://github.com/eth-clients/eth2-networks/raw/master/shared/prater/genesis.ssz
   fi
-else # Not the CL / beacon
-  exec "$@"
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+  exec "$@" "--genesis-state=$GENESIS" ${__rapid_sync} ${__prune} ${__mev_boost} ${CL_EXTRAS}
+elif [[ "$*" =~ --sepolia ]]; then
+GENESIS=/var/lib/prysm/genesis.ssz
+  if [ ! -f "$GENESIS" ]; then
+    echo "Fetching genesis file for Sepolia testnet"
+    curl -fsSL -o "$GENESIS" https://github.com/eth-clients/merge-testnets/raw/main/sepolia/genesis.ssz
+  fi
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+  exec "$@" "--genesis-state=$GENESIS" ${__rapid_sync} ${__prune} ${__mev_boost} ${CL_EXTRAS}
+else
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+  exec "$@" ${__network} ${__rapid_sync} ${__prune} ${__mev_boost} ${CL_EXTRAS}
 fi
