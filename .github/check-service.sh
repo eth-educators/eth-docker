5722daf374b05d696e2c44397df6da9a0b266367
Yorick Downe <yorickdowne@gmail.com>
2023-09-14 19:17:56 +0000

    Nethermind executable names for v1.21

--- .github/check-service.sh
+++ .github/check-service.sh
@@ -4,12 +4,10 @@ __service=$1
 
 __containerID=$(docker-compose ps -q "${__service}")
 
-__initial_restart_count=$(docker inspect --format '{{ .RestartCount }}' "$__containerID")
+__restart_count=$(docker inspect --format '{{ .RestartCount }}' "$__containerID")
 __is_running=$(docker inspect --format '{{ .State.Running }}' "$__containerID")
-sleep 10
-__final_restart_count=$(docker inspect --format '{{ .RestartCount }}' "$__containerID")
 
-if [ "$__is_running" != "true" ] || [ "$__final_restart_count" -gt "$__initial_restart_count" ]; then
+if [ "$__is_running" != "true" ] || [ "$__restart_count" -gt 1 ]; then
   echo "$__service is either not running or continuously restarting"
   docker-compose ps "${__service}"
   docker-compose logs "${__service}"
