3babf7ae6e38e4d5d1fa840b5d4b54f52efcf914
yorickdowne <71337066+yorickdowne@users.noreply.github.com>
2023-10-29 18:57:49 -0400

    Prep postgres migration (#1581)
    * Prep postgres migration
    
    * Rework error handling

--- web3signer/docker-entrypoint.sh
+++ web3signer/docker-entrypoint.sh
@@ -6,6 +6,13 @@ if [ "$(id -u)" = '0' ]; then
   exec gosu web3signer docker-entrypoint.sh "$@"
 fi
 
+if [ -f /var/lib/web3signer/.migration_fatal_error ]; then
+    echo "An error occured during slashing protection database migration, that makes it unsafe to start Web3signer."
+    echo "Until this is manually remedied, Web3signer will refuse to start up."
+    echo "Aborting."
+    exit 1
+fi
+
 /flyway/flyway migrate -url=jdbc:postgresql://postgres/web3signer -user=postgres -password=postgres -locations=filesystem:/opt/web3signer/migrations/postgresql
 
 exec "$@"
