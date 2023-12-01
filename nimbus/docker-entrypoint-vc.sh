1aa35b09578de8eae680f2c5e670e6597be7b220
Yorick Downe <yorickdowne@gmail.com>
2023-12-01 14:27:08 +0000

    Web3signer wait loop for Nimbus

--- nimbus/docker-entrypoint-vc.sh
+++ nimbus/docker-entrypoint-vc.sh
@@ -37,6 +37,15 @@ __log_level="--log-level=${LOG_LEVEL^^}"
 # Web3signer URL
 if [ "${WEB3SIGNER}" = "true" ]; then
   __w3s_url="--web3-signer-url=http://web3signer:9000"
+  while true; do
+    if curl -s -m 5 http://web3signer:9000 &> /dev/null; then
+        echo "Web3signer is up, starting Nimbus"
+        break
+    else
+        echo "Waiting for Web3signer to be reachable..."
+        sleep 5
+    fi
+  done
 else
   __w3s_url=""
 fi
