b21e489643cc49257d1386f899356e8ec5b695e4
Nico Flaig <nflaig@protonmail.com>
2024-01-29 10:43:20 +0100

    Update deprecated CLI flags

--- lodestar/docker-entrypoint.sh
+++ lodestar/docker-entrypoint.sh
@@ -56,7 +56,7 @@ fi
 
 # Check whether we should use MEV Boost
 if [ "${MEV_BOOST}" = "true" ]; then
-  __mev_boost="--builder --builder.urls=${MEV_NODE:-http://mev-boost:18550}"
+  __mev_boost="--builder --builder.url=${MEV_NODE:-http://mev-boost:18550}"
   echo "MEV Boost enabled"
 else
   __mev_boost=""
