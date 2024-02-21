ef5406cee8cb4074525980bfe509aec0e64bbe48
yorickdowne <71337066+yorickdowne@users.noreply.github.com>
2024-02-21 08:31:58 -0500

    Remove pre-merge legacy code, remove compose V1 code (#1603)

--- .github/check-service.sh
+++ .github/check-service.sh
@@ -2,18 +2,18 @@
 
 __service=$1
 
-__containerID=$(docker-compose ps -q "${__service}")
+__containerID=$(docker compose ps -q "${__service}")
 
 __restart_count=$(docker inspect --format '{{ .RestartCount }}' "$__containerID")
 __is_running=$(docker inspect --format '{{ .State.Running }}' "$__containerID")
 
 if [ "$__is_running" != "true" ] || [ "$__restart_count" -gt 1 ]; then
   echo "$__service is either not running or continuously restarting"
-  docker-compose ps "${__service}"
-  docker-compose logs "${__service}"
+  docker compose ps "${__service}"
+  docker compose logs "${__service}"
   exit 1
 else
   echo "$__service is running"
-  docker-compose ps "${__service}"
+  docker compose ps "${__service}"
   exit 0
 fi
