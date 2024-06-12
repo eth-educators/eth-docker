a8153d332bcfe602dcfef90c0d7317e41ba0ed86
threehonor <pengqi@email.cn>
2024-06-12 14:09:19 +0800

    chore: fix some comments
    Signed-off-by: threehonor <pengqi@email.cn>

--- grandine/docker-entrypoint.sh
+++ grandine/docker-entrypoint.sh
@@ -12,7 +12,7 @@ if [ -n "${JWT_SECRET}" ]; then
 fi
 
 if [[ -O "/var/lib/grandine/ee-secret" ]]; then
-  # In case someone specificies JWT_SECRET but it's not a distributed setup
+  # In case someone specifies JWT_SECRET but it's not a distributed setup
   chmod 777 /var/lib/grandine/ee-secret
 fi
 if [[ -O "/var/lib/grandine/ee-secret/jwtsecret" ]]; then
