04a95c0c246fba9a086f2e9a4a5922806da677b9
yorickdowne <71337066+yorickdowne@users.noreply.github.com>
2024-08-31 15:36:57 -0400

    Prysm persist w3s keys loaded via keymanager (#1917)

--- prysm/docker-entrypoint-vc.sh
+++ prysm/docker-entrypoint-vc.sh
@@ -47,7 +47,13 @@ fi
 
 # Web3signer URL
 if [ "${WEB3SIGNER}" = "true" ]; then
-  __w3s_url="--validators-external-signer-url http://web3signer:9000 --validators-external-signer-public-keys http://web3signer:9000/api/v1/eth2/publicKeys"
+  __w3s_url="--validators-external-signer-url http://web3signer:9000 \
+  --validators-external-signer-public-keys http://web3signer:9000/api/v1/eth2/publicKeys \
+  --validators-external-signer-key-file=/var/lib/prysm/w3s-keys.txt"
+
+  if [ ! -f /var/lib/prysm/w3s-keys.txt ]; then
+    touch /var/lib/prysm/w3s-keys.txt
+  fi
 else
   __w3s_url=""
 fi
