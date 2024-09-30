4f457cf5cb934310176974fc07f568dee59f1eb3
Yorick <yorickdowne@gmail.com>
2024-09-30 17:42:12 +0000

    Revert "Fix Lighthouse v6 (#1942)"
    This reverts commit 07aca7d04c7e4f39628692a7444ba1b074b60b54.

--- lighthouse/docker-entrypoint.sh
+++ lighthouse/docker-entrypoint.sh
@@ -87,7 +87,7 @@ fi
 
 if [ "${IPV6}" = "true" ]; then
   echo "Configuring Lighthouse to listen on IPv6 ports"
-  __ipv6="--listen-address :: --port6 ${CL_IPV6_P2P_PORT:-9090}"
+  __ipv6="--listen-address :: --port6 ${CL_P2P_PORT:-9000} --quic-port6 ${CL_QUIC_PORT:-9001}"
 # ENR discovery on v6 is not yet working, likely too few peers. Manual for now
   __ipv6_pattern="^[0-9A-Fa-f]{1,4}:" # Sufficient to check the start
   set +e
